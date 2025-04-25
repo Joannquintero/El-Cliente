69b3aaa3ad72df6c5463e14891533549825c8603
joann.quintero <joann.quintero@kpgin.com>
2025-04-24 22:01:19 -0500

    feat: creacion de la clase DataContext

---
+++ El-Cliente.Api/Data/DataContext.cs
@@ -0,0 +1,28 @@
+﻿using El_Cliente.Shared.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace El_Cliente.Api.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Availability> Availability { get; set; }
+        public DbSet<Balance> Balances { get; set; }
+        public DbSet<Branch> Branchs { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Registration> Registrations { get; set; }
+        public DbSet<Transaction> Transactions { get; set; }
+        public DbSet<Visit> Visits { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Branch>().HasIndex(x => x.Name).IsUnique();
+            modelBuilder.Entity<Product>().HasIndex(x => x.Name).IsUnique();
+        }
+    }
+}
--- El-Cliente.Api/Program.cs
+++ El-Cliente.Api/Program.cs
@@ -1,8 +1,23 @@
+using El_Cliente.Api.Data;
+using Microsoft.EntityFrameworkCore;
+using System.Text.Json.Serialization;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddJsonOptions(x => x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
+builder.Services.AddDbContext<DataContext>(x => x.UseSqlServer("name=DefaultConnection"));
+//builder.Services.AddTransient<SeedDb>();
+//builder.Services.AddScoped<IFileStorage, FileStorage>();
+//builder.Services.AddScoped<IUserHelper, UserHelper>();
+//builder.Services.AddScoped<IOrdersHelper, OrdersHelper>();
+
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -17,9 +32,19 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
+//app.UseBlazorFrameworkFiles();
+app.UseStaticFiles();
+app.UseRouting();
+app.MapRazorPages();
+app.UseAuthentication();
 app.UseAuthorization();
-
 app.MapControllers();
+app.MapFallbackToFile("index.html");
+
+app.UseCors(x => x
+    .AllowAnyMethod()
+    .AllowAnyHeader()
+    .SetIsOriginAllowed(origin => true)
+    .AllowCredentials());
 
 app.Run();
