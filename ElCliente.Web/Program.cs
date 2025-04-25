ea5772ef8e9462e55981c88160517b13f152ccb3
joann.quintero <joann.quintero@kpgin.com>
2025-04-25 16:05:09 -0500

    feat: agregando libreria MudBlazor

--- ElCliente.Web/Program.cs
+++ ElCliente.Web/Program.cs
@@ -3,6 +3,7 @@ using ElCliente.Web;
 using ElCliente.Web.Repository;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -11,5 +12,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddSingleton(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7135/") });
 builder.Services.AddScoped<IRepository, Repository>();
 builder.Services.AddSweetAlert2();
+builder.Services.AddMudServices();
 
 await builder.Build().RunAsync();
